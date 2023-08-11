55ee6dcac7274155b400dc194535bfb13c902c7b
김지선 <129829326+k1mj15un@users.noreply.github.com>
2023-08-11 16:12:56 +0900

    Add files via upload

---
+++ a3.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    // nav
+    $(".nav > ul > li").mouseover(function(){
+        $(this).find(".submenu").stop().slideDown();
+    });
+    $(".nav > ul > li").mouseout(function(){
+        $(this).find(".submenu").stop().slideUp();
+    });
+
+    // slider
+    let currentIndex = 0;
+    $(".slider").hide().first().show();
+
+    setInterval(function(){
+        let nextIndex = (currentIndex + 1) % 3;
+
+        $(".slider").eq(currentIndex).fadeOut(1200);
+        $(".slider").eq(nextIndex).fadeIn(1200);
+
+        currentIndex = nextIndex;
+    }, 3000);
+
+    // tab
+    const tabBtn = $(".info-btn > a");
+    const tabCont = $(".info-cont > div");
+    tabCont.hide().eq(0).show();
+
+    tabBtn.on("click", function(){
+        const index = $(this).index();
+
+        $(this).addClass("active").siblings().removeClass("active");
+        tabCont.eq(index).show().siblings().hide();
+    });
+
+    // popup
+    $(".popup-btn").click(function(){
+        $(".popup-view").show();
+    });
+    $(".popup-close").click(function(){
+        $(".popup-view").hide();
+    });
+});
