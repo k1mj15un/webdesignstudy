275207be9e1707dc27fc70b8cd6c5639e13971a5
김지선 <129829326+k1mj15un@users.noreply.github.com>
2023-08-13 21:14:31 +0900

    Add files via upload

--- d-4.js
+++ d-4.js
@@ -1,26 +1,10 @@
 $(document).ready(function(){
-    // nav
     $(".nav > ul > li").mouseover(function(){
         $(".nav > ul > li > ul").stop().fadeIn();
-        $("#main").addClass("on");
+        $(".main").addClass("on");
     });
     $(".nav > ul > li").mouseout(function(){
         $(".nav > ul > li > ul").stop().fadeOut();
-        $("#main").removeClass("on");
+        $(".main").removeClass("on");
     });
-
-    let currentIndex = 0;
-    $(".sliderWrap").append($(".slider").first().clone(true));
-
-    setInterval(function(){
-        currentIndex++;
-        $(".sliderWrap").animate({marginTop: -currentIndex * 400 + "px"}, 600);
-
-        if(currentIndex == 3){
-            setTimeout(function(){
-                $(".sliderWrap").animate({marginTop: 0}, 0);
-                currentIndex = 0;
-            }, 700);
-        }
-    }, 3000);
 });
