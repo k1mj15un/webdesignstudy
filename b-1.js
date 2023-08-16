8b0a87ee335164ea6e6419176fc2d26c72a41c98
김지선 <129829326+k1mj15un@users.noreply.github.com>
2023-08-17 01:41:53 +0900

    Add files via upload
    b유형 메뉴, 슬라이드, 탭메뉴 연습

--- b-1.js
+++ b-1.js
@@ -1,46 +1,36 @@
 $(document).ready(function(){
     // nav
     $(".nav > ul > li").mouseover(function(){
-        $(".nav > ul > li > ul").stop().slideDown();
+        $(".submenu").stop().slideDown();
     });
     $(".nav > ul > li").mouseout(function(){
-        $(".nav > ul > li > ul").stop().slideUp();
+        $(".submenu").stop().slideUp();
     });
 
     // slider
     let currentIndex = 0;
-    $(".sliderWrap").append($(".slider").first().clone());
+    $(".sliderWrap").append($(".slider").first().clone(true));
 
     setInterval(function(){
         currentIndex++;
-        $(".sliderWrap").animate({marginLeft : -currentIndex * 100 + "%"}, 300);
+        $(".sliderWrap").animate({marginLeft: -currentIndex * 100 + "%"}, 600);
 
         if(currentIndex == 3){
             setTimeout(function(){
                 $(".sliderWrap").animate({marginLeft: 0}, 0);
-                currentIndex = 0;
             }, 700);
         }
     }, 3000);
 
     // tab
-    let tabBtn = $(".tab-btn > ul > li");
-    let tabCont = $(".tab-cont > div");
-    tabCont.hide().eq(0).show();
+    const tabBtn = $(".tab > h3 > a"); //탭 버튼 설정
+    const tabCont = $(".tab-cont > ul"); //탭 콘텐츠 설정
+    tabCont.hide().eq(0).show(); //탭 콘텐츠 숨기고 첫번째 콘텐츠만 보여줌
 
-    tabBtn.click(function(){
-        const index = $(this).index();
+    tabBtn.on("click", function(){ //버튼 클릭하면
+        const index = $(this).index(); //클릭한 버튼의 번호 저장
 
-        $(this).addClass("active").siblings().removeClass("active");
-        tabCont.eq(index).show().siblings().hide();
-    });
-
-
-    // popup
-    $(".popup-btn").click(function(){
-        $(".popup-view").show();
-    });
-    $(".popup-close").click(function(){
-        $(".popup-view").hide();
+        $(this).addClass("active").siblings().removeClass("active"); //클릭한 버튼에 클래스 추가, 나머지 제거
+        tabCont.eq(index).show(0).siblings().hide(); //클릭한 버튼 순서에 맞는 콘텐츠에 클래스 추가, 나머지 숨김
     });
 });
