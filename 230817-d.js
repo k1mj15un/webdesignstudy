58b7eefbb13cc07cd4bf880bf8fb45db90e7366a
김지선 <129829326+k1mj15un@users.noreply.github.com>
2023-08-17 16:22:12 +0900

    Add files via upload
    d유형 전체연습

---
+++ 230817-d.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+    // nav
+    $(".nav > ul > li").mouseover(function(){
+        $(".nav > ul > li > ul").stop().fadeIn();
+        $("#main").addClass("on");
+    });
+    $(".nav > ul > li").mouseout(function(){
+        $(".nav > ul > li > ul").stop().fadeOut();
+        $("#main").removeClass("on");
+    });
+
+    // slider
+    let currentIndex = 0;
+
+    setInterval(function(){
+        let nextIndex = (currentIndex + 1) % 3;
+
+        $(".slider").eq(currentIndex).fadeOut(1200);
+        $(".slider").eq(nextIndex).fadeIn(1200);
+
+        currentIndex = nextIndex;
+    }, 3000);
+
+    // tab
+    const tabBtn = $(".tab-btn > a");
+    const tabCont = $(".tab-cont > ul");
+    tabCont.hide().eq(0).show();
+
+    tabBtn.on("click", function(){
+        const index = $(this).index();
+        $(this).addClass("active").siblings().removeClass("active");
+        tabCont.eq(index).show().siblings().hide();
+    });
+
+    // popup
+    $(".popup-btn").click(function(){
+        $(".popup-view").show();
+    });
+    $(".popup-close").click(function(){
+        $(".popup-view").hide();
+    });
+});
