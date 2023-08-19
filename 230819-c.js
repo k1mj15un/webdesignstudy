f04baf356f7f3bbe5c1d5fe21797a4d83537d676
김지선 <129829326+k1mj15un@users.noreply.github.com>
2023-08-19 23:28:35 +0900

    Add files via upload
    c유형 연습

---
+++ 230819-c.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    // nav
+    $(".nav > ul > li").mouseover(function(){
+        $(this).find(".submenu").stop().slideDown();
+    });
+    $(".nav > ul > li").mouseout(function(){
+        $(this).find(".submenu").stop().slideUp();
+    });
+
+    // slider
+    let currentIndex = 0;
+
+    setInterval(function(){
+        let nextIndex = (currentIndex + 1) % 3;
+
+        $(".slider").eq(currentIndex).fadeOut(1200);
+        $(".slider").eq(nextIndex).fadeIn(1200);
+
+        currentIndex = nextIndex;
+    }, 3000);
+
+    // tab
+    const tabBtn = $(".tab-btn > a");
+    const tabCont = $(".tab-cont > ul");
+    tabCont.hide().eq(0).show();
+
+    tabBtn.click(function(){
+        const index = $(this).index();
+
+        $(this).addClass("active").siblings().removeClass("active");
+        tabCont.eq(index).show().siblings().hide();
+    });
+});
